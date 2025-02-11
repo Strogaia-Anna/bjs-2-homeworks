7a28d6f167a47f88b012d7c63b562bf43fcedda8
Strogaya Anna <strogaia.ania@gmail.com>
2025-02-11 14:43:44 +0300

    исправление замечаний

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,19 +2,16 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let discriminant = b ** 2 - 4 * a * c;
-  if (discriminant < 0) {
-    return arr;
-  } else if (discriminant === 0) {
+  if (discriminant === 0) {
     let root = -b / (2 * a);
     arr.push(root);
-    return arr;
-  }  else {
+  }  else if (discriminant > 0) {
     let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
     let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
     arr.push(root1);
     arr.push(root2);
-    return arr;
   }
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
